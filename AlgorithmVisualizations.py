16f6d873f25233d71761871ca31488995f78cda6
John Canning <John.Canning@shakumant.com>
2020-06-04 21:27:36 -0700

    Revert "Add a full_msg variable"
    This reverts commit d5ffa29c58907016f54cc40dcff6827918841b88.

--- AlgorithmVisualizations.py
+++ AlgorithmVisualizations.py
@@ -61,8 +61,7 @@ def showVisualizations(   # Display a set of VisualizationApps in a ttk.Notebook
                           padding=[12, TAB_FONT[1] * 5 // 8, 12, 2])
     notebook = ttk.Notebook(top)
     intro = ttk.Frame(notebook)
-    full_msg = intro_msg
-    for line in full_msg.split('\n'):
+    for line in intro_msg.split('\n'):
         URLs = [m for m in URL_pattern.finditer(line)]
         if URLs:
             frame = ttk.Frame(intro)
